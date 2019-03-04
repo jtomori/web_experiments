6872123a18db2454329feb10e1647ceb4bd5958b
jtomori <tomori.juraj@gmail.com>
2019-03-04 01:42:15 +0100

    updated styles, re-structured js code

--- sdb_he_vr/panorama.js
+++ sdb_he_vr/panorama.js
@@ -1,15 +1,32 @@
+'use strict';
+
 // globals
-var camera, scene, renderer, material;
-var isUserInteracting = false,
+let camera, scene, renderer;
+
+// orbit controls
+let isUserInteracting = false,
 onMouseDownMouseX = 0, onMouseDownMouseY = 0,
 lon = 0, onMouseDownLon = 0,
 lat = 0, onMouseDownLat = 0,
-phi = 0, theta = 0,
-panoramaNumber = 0;
+phi = 0, theta = 0;
+
+// panoramas
+let panorama_number = 0;
+
+let panorama_paths = [
+    "img/ren_v003_day_10.jpg",
+    "img/ren_v003_day_8.jpg",
+    "img/ren_v003_day_9.jpg",
+    "img/ren_v003_night_10.jpg",
+    "img/ren_v003_night_8.jpg",
+    "img/ren_v003_night_9.jpg"
+];
+
+let panorama_textures = [];
 
 function init() {
     // div container
-    var container;
+    let container;
     container = document.getElementById( 'container' );
 
     // cam
@@ -19,17 +36,20 @@ function init() {
     // scene
     scene = new THREE.Scene();
     
-    var geometry = new THREE.SphereBufferGeometry( 500, 64, 64 );
+    // sphere with flipped normals
+    let geometry = new THREE.SphereBufferGeometry( 500, 64, 64 );
     geometry.scale( - 1, 1, 1 );
     
-    var tex = new THREE.TextureLoader().load( 'img/ren_v003_day_8.jpg' );
+    // initial texture
+    let tex = new THREE.TextureLoader().load( panorama_paths[panorama_number] );
     tex.minFilter = THREE.LinearFilter; // poles pinching fix
+    panorama_textures[0] = tex;
 
-    material = new THREE.MeshBasicMaterial( {
+    let material = new THREE.MeshBasicMaterial( {
         map: tex
     } );
     
-    var mesh = new THREE.Mesh( geometry, material );
+    let mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
     
     
@@ -40,39 +60,36 @@ function init() {
     container.appendChild( renderer.domElement );
     
     // events
-    document.addEventListener( 'mousedown', onPointerStart, false );
-    document.addEventListener( 'mousemove', onPointerMove, false );
-    document.addEventListener( 'mouseup', onPointerUp, false );
-    document.addEventListener( 'touchstart', onPointerStart, false );
-    document.addEventListener( 'touchmove', onPointerMove, false );
-    document.addEventListener( 'touchend', onPointerUp, false );
-    window.addEventListener( 'resize', onWindowResize, false );
+    document.addEventListener( 'mousedown', onPointerStart);
+    document.addEventListener( 'mousemove', onPointerMove);
+    document.addEventListener( 'mouseup', onPointerUp);
+    document.addEventListener( 'touchstart', onPointerStart);
+    document.addEventListener( 'touchmove', onPointerMove);
+    document.addEventListener( 'touchend', onPointerUp);
+    window.addEventListener( 'resize', onWindowResize);
 
-    // switching
-    var switchBtn = document.querySelectorAll('.controls li[class*="switch"]');
+    // switching images
+    let switch_buttons = document.querySelectorAll('.controls li[class*="switch"]');
 
-    var panoramasArray = [
-        "img/ren_v003_day_8.jpg", 
-        "img/ren_v003_day_9.jpg", 
-        "img/ren_v003_day_10.jpg",
-        "img/ren_v003_night_8.jpg", 
-        "img/ren_v003_night_9.jpg", 
-        "img/ren_v003_night_10.jpg"
-    ];
+    for (let i = 0; i < switch_buttons.length; i++) {
+        switch_buttons[i].onclick = function (event) {
+            let panoramas_len = panorama_paths.length;
 
-    for (let i = 0; i < switchBtn.length; i++) {
-        switchBtn[i].onclick = (e) => {
-            let j = panoramasArray.length;
+            if (event.target.className === "controls_switch-next")
+                panorama_number = (panorama_number + 1) % panoramas_len;
+            else
+                panorama_number = (panorama_number - 1) % panoramas_len;
 
-            panoramaNumber = ~e.target.className.indexOf("controls__switch-next") ? (panoramaNumber + 1) % j : (panoramaNumber - 1) % j;
-            material.map = new THREE.TextureLoader().load(panoramasArray[Math.abs(panoramaNumber)]);
+            // load texture
+            //let new_tex = new THREE.TextureLoader().load( panorama_paths[ Math.abs(panorama_number) ] );
+            //new_tex.minFilter = THREE.LinearFilter;
+
+            let new_tex = panorama_textures[ Math.abs(panorama_number) ];
+
+            // assign new texture
+            material.map = new_tex;
         };
     }
-}
-
-function animate() {
-    requestAnimationFrame( animate );
-    update();
 }
 
 function update() {
@@ -92,8 +109,23 @@ function update() {
     renderer.render( scene, camera );
 }
 
+function animate() {
+    requestAnimationFrame( animate );
+    update();
+}
+
 init();
 animate();
+loadTextures();
+
+// loads all textures
+function loadTextures() {
+    for (let i = 1; i < panorama_paths.length; i++) {
+        let new_tex = new THREE.TextureLoader().load( panorama_paths[i] )
+        new_tex.minFilter = THREE.LinearFilter;
+        panorama_textures[i] = new_tex;
+    }
+}
 
 // events
 function onWindowResize() {
@@ -102,10 +134,11 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
+// orbit controls
 function onPointerStart( event ) {
     isUserInteracting = true;
-    var clientX = event.clientX || event.touches[ 0 ].clientX;
-    var clientY = event.clientY || event.touches[ 0 ].clientY;
+    let clientX = event.clientX || event.touches[ 0 ].clientX;
+    let clientY = event.clientY || event.touches[ 0 ].clientY;
     onMouseDownMouseX = clientX;
     onMouseDownMouseY = clientY;
     onMouseDownLon = lon;
@@ -114,8 +147,8 @@ function onPointerStart( event ) {
 
 function onPointerMove( event ) {
     if ( isUserInteracting === true ) {
-        var clientX = event.clientX || event.touches[ 0 ].clientX;
-        var clientY = event.clientY || event.touches[ 0 ].clientY;
+        let clientX = event.clientX || event.touches[ 0 ].clientX;
+        let clientY = event.clientY || event.touches[ 0 ].clientY;
         lon = ( onMouseDownMouseX - clientX ) * 0.1 + onMouseDownLon;
         lat = ( clientY - onMouseDownMouseY ) * 0.1 + onMouseDownLat;
     }
